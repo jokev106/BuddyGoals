16f686d5b436dff9b70c110692df1303ab55b710
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-07-13 14:58:11 +0700

    feat: Updating onboarding feature

--- BuddyGoals/BuddyGoals/Views/OnboardingView.swift
+++ BuddyGoals/BuddyGoals/Views/OnboardingView.swift
@@ -10,94 +10,96 @@ import SwiftUI
 struct OnboardingView: View {
     var body: some View {
         
-        VStack{
-            //Onboarding TITLE
-            Text(" Welcome to\nBuddy Goals!")
-                .fontWeight(.bold)
-                .lineSpacing(10)
-                .font(.system(size: 50).bold())
-                .foregroundColor(primary900)
-            
-            Spacer()
-                .frame( height: 60)
-            
-            //Pict and text 1
-            HStack{
-                Image("Onboarding1")
-                    .resizable()
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    .frame(width: 120, height: 120)
-                VStack{
-                    Text("Achieve Goals with your Buddy")
-                        .fontWeight(.bold)
-                        .font(.system(size: 16))
-                        .padding(.bottom, 1.0)
-                        .frame(width: 250, alignment: .leading)
-                    Text("Invite a friend, colleagues, or significant others as an accountability partner to help you achieve your Goal.")
-                        .font(.system(size: 12))
-                        .padding(.horizontal, 1.0)
-                        .frame(width: 250, alignment: .leading)
-                }
-            }
-            Spacer()
-                .frame( height: 20)
-            //pict and text 2
-            HStack{
-                Image("Onboarding2")
-                    .resizable()
-                    .padding(.all)
-                    .frame(width: 115, height: 115)
-                VStack{
-                    Text("Break down Goals")
-                        .fontWeight(.bold)
-                        .font(.system(size: 16))
-                        .padding(.bottom, 1.0)
-                        .frame(width: 250, alignment: .leading)
-                    Text("Small bites, goals are easier to do if broken down to Plans and Actions.")
-                        .font(.system(size: 12))
-                        .padding(.horizontal, 1.0)
-                        .frame(width: 250, alignment: .leading)
-                }
-            }
-            Spacer()
-                .frame( height: 20)
-            //pict and text 3
-            HStack{
-                Image("development")
-                    .resizable()
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    .frame(width: 110, height: 110)
-                VStack{
-                    Text("Improve everyday")
-                        .fontWeight(.bold)
-                        .font(.system(size: 16))
-                        .padding(.bottom, 1.0)
-                        .frame(width: 250, alignment: .leading)
-                    Text("Improve yourself continuously every day and be the best version of yourself.")
-                        .font(.system(size: 12))
-                        .padding(.horizontal, 1.0)
-                        .frame(width: 250, alignment: .leading)
-                        
-                }
-            }
-            Spacer()
-                .frame( height: 90)
-            
-            Button {
-                
-            } label: {
-                Text("Get Started")
+        GeometryReader{ geo in
+            VStack{
+                //Onboarding TITLE
+                Text(" Welcome to\nBuddy Goals!")
                     .fontWeight(.bold)
-                    .padding(16)
-                    .frame(maxWidth: .infinity)
-                    .background(primary900)
-                    .foregroundColor(.white)
-                    .cornerRadius(16)
-                    .padding(.horizontal, 16)
-            }
+                    .lineSpacing(10)
+                    .font(.system(size: 50).bold())
+                    .foregroundColor(primary900)
+                
+                Spacer()
+                    .frame( height: 70)
+                
+                //Pict and text 1
+                HStack{
+                    Image("Onboarding1")
+                        .resizable()
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 120, height: 120)
+                    VStack{
+                        Text("Achieve Goals with your Buddy")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .padding(.bottom, 1.0)
+                            .frame(width: 250, alignment: .leading)
+                        Text("Invite a friend, colleagues, or significant others as an accountability partner to help you achieve your Goal.")
+                            .font(.system(size: 12))
+                            .padding(.horizontal, 1.0)
+                            .frame(width: 250, alignment: .leading)
+                    }
+                }
+                Spacer()
+                    .frame( height: 20)
+                //pict and text 2
+                HStack{
+                    Image("Onboarding2")
+                        .resizable()
+                        .padding(.all)
+                        .frame(width: 115, height: 115)
+                    VStack{
+                        Text("Break down Goals")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .padding(.bottom, 1.0)
+                            .frame(width: 250, alignment: .leading)
+                        Text("Small bites, goals are easier to do if broken down to Plans and Actions.")
+                            .font(.system(size: 12))
+                            .padding(.horizontal, 1.0)
+                            .frame(width: 250, alignment: .leading)
+                    }
+                }
+                Spacer()
+                    .frame( height: 20)
+                //pict and text 3
+                HStack{
+                    Image("development")
+                        .resizable()
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 110, height: 110)
+                    VStack{
+                        Text("Improve everyday")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .padding(.bottom, 1.0)
+                            .frame(width: 250, alignment: .leading)
+                        Text("Improve yourself continuously every day and be the best version of yourself.")
+                            .font(.system(size: 12))
+                            .padding(.horizontal, 1.0)
+                            .frame(width: 250, alignment: .leading)
+                            
+                    }
+                }
+                Spacer()
+                    .frame( height: 120)
+                
+                Button {
+                    
+                } label: {
+                    Text("Get Started")
+                        .fontWeight(.bold)
+                        .padding(16)
+                        .frame(maxWidth: .infinity)
+                        .background(primary900)
+                        .foregroundColor(.white)
+                        .cornerRadius(16)
+                        .padding(.horizontal, 16)
+                }
 
+            }
+            
         }
-        
     }
 }
 
