917964a8084177194c54a6e45b14b0b5f977b85d
gusde27 <gusdechang24@gmail.com>
2022-07-20 15:47:15 +0800

    feat: first develop UI BuddyView

---
+++ BuddyGoals/BuddyGoals/Views/Companion/BuddyView.swift
@@ -0,0 +1,20 @@
+//
+//  BuddyView.swift
+//  BuddyGoals
+//
+//  Created by I Gede Bagus Wirawan on 20/07/22.
+//
+
+import SwiftUI
+
+struct BuddyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BuddyView_Previews: PreviewProvider {
+    static var previews: some View {
+        BuddyView()
+    }
+}
