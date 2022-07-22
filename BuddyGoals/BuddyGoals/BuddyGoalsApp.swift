fe8f34a57b3f6241d25f7f725d3d90aad85967fa
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-07-22 22:04:02 +0700

    feat: Onboarding UI update fixing

--- BuddyGoals/BuddyGoals/BuddyGoalsApp.swift
+++ BuddyGoals/BuddyGoals/BuddyGoalsApp.swift
@@ -15,7 +15,7 @@ struct BuddyGoalsApp: App {
 
     var body: some Scene {
         WindowGroup {
-            MainView()
+            IntroView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(activityToday)
                 .environmentObject(gvm)
